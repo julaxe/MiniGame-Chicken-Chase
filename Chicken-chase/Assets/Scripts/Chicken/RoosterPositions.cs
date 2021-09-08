cd642f80e842b516f101961ec6596e1675d66478
Julaxe <julaxe8@gmail.com>
2021-09-08 11:59:53 -0400

    animations fixed, build for emulator done, change on settings
    some animations had the position fixed, now it only moves the elements inside the object but not fixed the position.

---
+++ Chicken-chase/Assets/Scripts/Chicken/RoosterPositions.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoosterPositions : MonoBehaviour
+{
+    public List<Vector3> positions;
+    public List<Vector3> rotations;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
